49ba6961e68ac20ba1818a6b92a8c04c3ac8b0f2
Baubs <pathogenic@gmail.com>
2016-10-03 23:29:50 -0500

    implemented pyramid printing

---
+++ pyramid.js
@@ -0,0 +1,37 @@
+
+//gets the height
+var strHeight = document.getElementById("height").value;
+var height = parseInt(strHeight);
+
+//prints the pyramid
+var blocks = 2;
+var row = "";
+
+function printPyramid()
+{
+  //console.log("FUnction ran")//debug
+  //clears old pyramid
+  document.getElementById("pyramid").innerHTML = "";
+  blocks = 2;
+for(var i = 0; i < height - 1; i++)
+{
+  row = "";
+  for(var j = 0; j < height - blocks; j++)
+  {
+    row = row + ".";
+  }
+  for(var x = 0; x < blocks; x++)
+  {
+    row = row + "#";
+  }
+  blocks++;
+  var node = document.createElement("P");
+
+  var text = document.createTextNode(row);
+
+  node.appendChild(text);
+
+  document.getElementById("pyramid").appendChild(node);
+}
+
+}
